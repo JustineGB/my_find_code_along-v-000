c1a2460a72a2be0d2bd22b19424cc33f67014d29
Justine Bricknell <justinegayton@gmail.com>
2018-06-25 16:43:41 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+        if yelid(collection[i])
+          return cllection[i]
+        end
+    i = + 1
+    end
 end
